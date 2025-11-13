e32eba877ea278d6766309b98842674417d767fc
Oscar <oscar.burman@mainter.com>
2025-11-13 16:20:50 +0100

    Functions to create, delete and get dashboard

--- lib/dashboard_api/dashboards/dashboards.ex
+++ lib/dashboard_api/dashboards/dashboards.ex
@@ -2,11 +2,31 @@ defmodule DashboardApi.Dashboards.Dashboards do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias DashboardApi.Repo
+  alias DashboardApi.Dashboards.Dashboards
+
   schema "dashboards" do
     field :name, :string
     belongs_to :system, DashboardApi.Dashboards.Systems
     has_many :dashboard_cards, DashboardApi.Dashboards.DashboardCards, foreign_key: :dashboard_id
     timestamps(type: :utc_datetime)
+  end
+
+  def create_dashboard(attrs) do
+    %Dashboards{}
+    |> Dashboards.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_dashboard(id) do
+    Repo.get(Dashboards, id)
+  end
+
+  def delete_dashboard(id) do
+    case Repo.get(Dashboards, id) do
+      nil -> {:error, :not_found}
+      dashboard -> Repo.delete(dashboard)
+    end
   end
 
   def changeset(dashboard, attrs) do
