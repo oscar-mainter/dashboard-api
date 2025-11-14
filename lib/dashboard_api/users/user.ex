1198de3e0967af5a754d81901642349638bba852
Oscar <oscar.burman@mainter.com>
2025-11-14 09:51:02 +0100

    Created user schema

---
+++ lib/dashboard_api/users/user.ex
@@ -0,0 +1,19 @@
+defmodule DashboardApi.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string, null: false
+    belongs_to :system, DashboardApi.Systems.System
+    has_many :dashboards, DashboardApi.Dashboards.Dashboard, foreign_key: user_id
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 1, max: 100, message: "name must be between 1 and 100 characters")
+    |> foreign_key_constraint(:system_id)
+  end
+end
