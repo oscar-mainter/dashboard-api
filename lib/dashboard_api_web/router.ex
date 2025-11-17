3286819f126da60a6dc4308e0e241aa2a580c188
Oscar <oscar.burman@mainter.com>
2025-11-17 10:04:50 +0100

    Changed routes and added a plugs to convert ids in parms to be integers

--- lib/dashboard_api_web/router.ex
+++ lib/dashboard_api_web/router.ex
@@ -3,24 +3,26 @@ defmodule DashboardApiWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug DashboardApiWeb.Plugs.ConvertToNumericParams
+  end
+
+  pipeline :validate_dashboard do
+    plug DashboardApiWeb.Plugs.ValidateSystemUser
+    plug DashboardApiWeb.Plugs.ValidateDashboard
   end
 
   get "/", DashboardApiWeb.PageController, :index
 
-  scope "/api", DashboardApiWeb do
-    pipe_through :api
-
-    scope "/dashboards/:system_id/:user_id" do
-      pipe_through DashboardApiWeb.Plugs.ValidateSystemUser
+  scope "/api/v1/systems/:system_id/users/:user_id/dashboards", DashboardApiWeb do
+    pipe_through [:api, DashboardApiWeb.Plugs.ValidateSystemUser]
 
       post "/", DashboardController, :create
       get "/", DashboardController, :index
       get "/:dashboard_id", DashboardController, :show
       delete "/:dashboard_id", DashboardController, :delete
-    end
 
-    scope "/dashboards/:system_id/:user_id/:dashboard_id/cards" do
-      pipe_through [DashboardApiWeb.Plugs.ValidateSystemUser, DashboardApiWeb.Plugs.ValidateDashboard]
+    scope "/:dashboard_id/cards" do
+      pipe_through :validate_dashboard
 
       post "/", DashboardCardController, :create
       patch "/:id", DashboardCardController, :update
